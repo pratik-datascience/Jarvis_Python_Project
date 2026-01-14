5acd7d9c8a3495a928617a15d5f0168de86111e9
Pratik Khobragade <Pratikkhobragade448@gmail.com>
2026-01-14 12:16:26 +0530

    Delete MegaProject01(JARVIS) directory

--- MegaProject01(JARVIS)/client.py
+++
@@ -1,7 +0,0 @@
-from openai import OpenAI
-
-client = OpenAI(
-    api_key= ""
-)
-
-
--- MegaProject01(JARVIS)/musicLibrary.py
+++
@@ -1,6 +0,0 @@
-
-music = {
-    "hood" : "https://www.youtube.com/watch?v=jjiC7PfoIaY&pp=ygUHaG9vZCBhbg%3D%3D" ,
-     "mockingbird" :   "https://www.youtube.com/watch?v=S9bCLPwzSC0&pp=ygULbW9ja2luZ2JpcmQ%3D"      }
-
-
